32d2a28dbebbd6a93f8548afa4c22bc2ddc20bc9
Antonio de Jesus Hernandez Piña <tonyhdezpi@gmail.com>
2022-09-27 20:50:28 -0500

    Uploading neccesary files

---
+++ App.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text>This is a new app</Text>
+    </View>
+  );
+};
+
+export default App;
